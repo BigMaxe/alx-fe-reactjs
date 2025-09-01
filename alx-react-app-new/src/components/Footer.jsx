6ba9e3944e70a5b5097b50d221e9e4c2144c77db
Yaoduye <yaoduyeogbuleka@gmail.com>
2025-09-01 15:15:38 +0100

    Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{ backgroundColor: 'lightgray', textAlign: 'center', padding: '10px', color: 'black', marginTop: '100px' }} >
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ padding: '20px', fontFamily: 'Arial, sans-serif', textDecorationLine: 'underline' }} >
+            <p style={{  color: 'red', fontSize: '20px', fontStyle: 'italic', fontWeight: 'bold' }}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
